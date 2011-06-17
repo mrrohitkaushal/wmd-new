1da93f13a54713fdee9a11d976c76d87f9ad3c87
balpha <balpha@localhost>
2011-06-17 11:19:39 +0200

    yesterdays fix created a new inconsistency between MarkdownSharp and Showadown regarding handling of headers

--- showdown.js
+++ showdown.js
@@ -685,10 +685,10 @@ var _DoHeaders = function(text) {
 	//	--------
 	//
 	text = text.replace(/^(.+)[ \t]*\n=+[ \t]*\n+/gm,
-		function(wholeMatch,m1){return "<h1>" + _RunSpanGamut(m1) + "</h1>";});
+		function(wholeMatch,m1){return "<h1>" + _RunSpanGamut(m1) + "</h1>\n\n";});
 
 	text = text.replace(/^(.+)[ \t]*\n-+[ \t]*\n+/gm,
-		function(matchFound,m1){return "<h2>" + _RunSpanGamut(m1) + "</h2>";});
+		function(matchFound,m1){return "<h2>" + _RunSpanGamut(m1) + "</h2>\n\n";});
 
 	// atx-style headers:
 	//  # Header 1
@@ -712,7 +712,7 @@ var _DoHeaders = function(text) {
 	text = text.replace(/^(\#{1,6})[ \t]*(.+?)[ \t]*\#*\n+/gm,
 		function(wholeMatch,m1,m2) {
 			var h_level = m1.length;
-			return "<h" + h_level + ">" + _RunSpanGamut(m2) + "</h" + h_level + ">";
+			return "<h" + h_level + ">" + _RunSpanGamut(m2) + "</h" + h_level + ">\n\n";
 		});
 
 	return text;
